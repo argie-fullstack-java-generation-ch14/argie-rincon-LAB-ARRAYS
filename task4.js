6e1e5675e594cafe920769c4f9cc46be894a2f0c
argierincon <argierincon@gmail.com>
2026-09-22 13:01:28 -0500

    feat: task 4 completed

--- task4.js
+++ task4.js
@@ -1,9 +1,18 @@
+// eliminar uno de los dos números duplicados en el array dado
 const arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-// ¡Escribe tu código debajo de esta línea!
+// ventana de vision de la compu
+let prev;
+let current;
 
+for (let i = 0; i < arr.length; i++) {
+    current = arr[i];
 
-
-// ¡Escribe tu código encima de esta línea!
+    if (current === prev) {
+        arr.splice(i, 1)
+        break;
+    }
+    prev = current;
+}
 
 arr.forEach(element => console.log(element))
