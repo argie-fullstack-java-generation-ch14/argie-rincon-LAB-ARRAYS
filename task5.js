a7a1f92fe5a32c3c12f5251b1181ad2e9d05377d
argierincon <argierincon@gmail.com>
2026-09-22 15:15:21 -0500

    feat: add manual loop alternative to join in task5

--- task5.js
+++ task5.js
@@ -3,3 +3,10 @@
 const arr0 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 console.log(arr0.join());
+
+// alternativa manual al join() - formar el string con un for tradicional
+let result = String(arr0[0]);
+for (let i = 1; i < arr0.length; i++) {
+    result += "," + arr0[i];
+}
+console.log(result);
