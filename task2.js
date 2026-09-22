5636a91a5ec24403d823f96961b7fa98fc0c03f5
argierincon <argierincon@gmail.com>
2026-09-22 12:40:55 -0500

    feat: task 2 completed

--- task2.js
+++ task2.js
@@ -1,9 +1,11 @@
+// invertir el orden del array dado
 const arr = [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-// ¡Escribe tu código debajo de esta línea!
+// con reverse
+arr.reverse();
 
-
-
-// ¡Escribe tu código encima de esta línea!
-
-arr.forEach(element => console.log(element))
+// con un for iterando desde el último índice hasta el primero
+for (let i = arr.length - 1; i >= 0; i--) {
+    const ele = arr[i];
+    console.log(ele);
+}
