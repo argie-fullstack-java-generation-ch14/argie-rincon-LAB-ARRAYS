0dd98f8d4968a5937de168d93b629d173c9e0f79
argierincon <argierincon@gmail.com>
2026-09-22 15:16:40 -0500

    feat: add manual loop alternative to concat in task6

--- task6.js
+++ task6.js
@@ -2,3 +2,13 @@ const arr1 = ["hello"]
 const arr2 = ["world"]
 
 console.log(arr1.concat(arr2));
+
+// alternativa manual al concat(): copiar ambos arrays con for
+const fusionado = [];
+for (let i = 0; i < arr1.length; i++) {
+    fusionado.push(arr1[i]);
+}
+for (let i = 0; i < arr2.length; i++) {
+    fusionado.push(arr2[i]);
+}
+console.log(fusionado);
